2e65404ae874d77156d2784af4bdc139e695ac5d
plapicola <p.lapicola@gmail.com>
2019-07-07 18:15:26 -0600

    Adds Olympian::oldest and Olympian::youngest to fetch filtered olympians for index endpoint and breaks baseline olympians index query in to private function for query composition

--- lib/koroibos/olympian.ex
+++ lib/koroibos/olympian.ex
@@ -38,13 +38,43 @@ defmodule Koroibos.Olympian do
   @spec all_with_medals() :: list()
   def all_with_medals do
     Repo.all(
-      from o in Olympian,
-      inner_join: s in assoc(o, :sport),
-      inner_join: t in assoc(o, :team),
-      left_join: medals in assoc(o, :medals),
-      group_by: [s.name, t.name, o.age, o.name, o.id],
-      order_by: [asc: o.id],
-      select: %{name: o.name, age: o.age, team: t.name, sport: s.name, total_medals_won: count(medals.id)}
+      from o in base_olympians_query(),
+      order_by: [asc: o.id]
     )
+  end
+
+  @doc """
+  Fetches the youngest olympian in the system and maps the relationship names and medal count to the result
+  """
+  @spec youngest() :: list()
+  def youngest do
+    Repo.all(
+      from o in base_olympians_query(),
+      order_by: [asc: o.age],
+      limit: 1
+    )
+  end
+
+  @doc """
+  Fetches the oldest olympian in the system and maps the relationship names and medal count to the result
+  """
+  @spec oldest() :: list()
+  def oldest do
+    Repo.all(
+      from o in base_olympians_query(),
+      order_by: [desc: o.age],
+      limit: 1
+    )
+  end
+
+  # Wrapper function to return the base query used in `all_with_medals/0`, `youngest/0`, and `oldest/0`
+  @spec base_olympians_query() :: Ecto.Query
+  defp base_olympians_query do
+    from o in Olympian,
+    inner_join: s in assoc(o, :sport),
+    inner_join: t in assoc(o, :team),
+    left_join: medals in assoc(o, :medals),
+    group_by: [s.name, t.name, o.age, o.name, o.id],
+    select: %{name: o.name, age: o.age, team: t.name, sport: s.name, total_medals_won: count(medals.id)}
   end
 end
