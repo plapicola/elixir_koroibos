a0eb9c3a5b5c9554aaff66e9930717e5f33bfcc9
plapicola <p.lapicola@gmail.com>
2019-07-11 16:31:48 -0600

    Removes static asset configuration from production config

--- config/prod.exs
+++ config/prod.exs
@@ -11,9 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :koroibos, KoroibosWeb.Endpoint,
   url: [scheme: "https", host: "ex-koroibos", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
-
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+  
 # Do not print debug messages in production
 config :logger, level: :info
 
